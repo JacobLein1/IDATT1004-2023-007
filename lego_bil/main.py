e2535c3d571df623d5ee3703e01cf56d585e1bd9
Anders <anders.morille@gmail.com>
2023-10-02 15:14:28 +0200

    La til lego mindstorms bil

---
+++ lego_bil/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.ev3devices import (ColorSensor, GyroSensor, InfraredSensor,
+                                 Motor, TouchSensor, UltrasonicSensor)
+from pybricks.hubs import EV3Brick
+from pybricks.media.ev3dev import ImageFile, SoundFile
+from pybricks.parameters import Button, Color, Direction, Port, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import DataLog, StopWatch, wait
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+
+# Write your program here.
+ev3.speaker.beep()
+
+
+class Robot:
+    def __init__(self):
+        logging.debug("Setting up...")
