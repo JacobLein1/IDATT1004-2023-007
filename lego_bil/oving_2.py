0a7d43d89334535552f54a91edce22c6d19b3375
Anders <anders.morille@gmail.com>
2023-10-13 12:04:37 +0200

    Good

--- lego_bil/oving_2.py
+++ lego_bil/oving_2.py
@@ -21,7 +21,8 @@ ev3 = EV3Brick()
 # black = 0
 threshold = 15
 
-speed = -250
+speed = -300
+turn_speed = -500
 
 
 # Write your program here.
@@ -30,35 +31,47 @@ motor_left = Motor(port=Port.A)
 motor_right = Motor(port=Port.B)
 current_color = ColorSensor(port=Port.S2)
 destroyer = Motor(port=Port.D)
-press_sensor1 = TouchSensor(port=Port.S1)
+on_off_sensor = TouchSensor(port=Port.S1)
+press_sensor = TouchSensor(port=Port.S4)
 
 
-engine = DriveBase(motor_left, motor_right, wheel_diameter=56, axle_track=100)
+engine = DriveBase(motor_right, motor_left, wheel_diameter=56, axle_track=100)
 
 
-def is_black(sensor: ColorSensor) -> bool:
-    return sensor.reflection() <= threshold
-
-
-destroyer_running = False
+is_running = False
 
 while True:
+    if on_off_sensor.pressed() and not is_running:
+        is_running = True
+        ev3.speaker.say("Excercize 2")
+        wait(2000)
+        engine.drive(-400, 0)
+    if press_sensor.pressed() and is_running:
+        engine.straight(-200)
+        engine.stop()
+        engine.turn(180)
+        engine.straight(-400)
+        engine.turn(-180)
+        engine.drive(-400, 0)
+    if is_running and on_off_sensor.pressed():
+        engine.stop()
+        ev3.speaker.say("Excercize 2 done")
+        break
+
+
+""" 
+while True:
     if is_black(current_color):
-        engine.drive(speed, -60)
-        wait(250)
+        engine.drive(speed, -5)
+        wait(10)
     else:
-        engine.drive(speed, 60)
+        engine.drive(speed, 5)
     wait(10)
     ev3.screen.clear()
     ev3.screen.draw_text(0, 20, "Is black?: " + str(is_black(current_color)))
     ev3.screen.draw_text(0, 40, "Light value: " +
                          str(current_color.reflection()))
 
-    if press_sensor1.pressed():
-        destroyer_running = not destroyer_running
-        wait(1000)
-
-    if destroyer_running:
-        destroyer.run(1000)
-    else:
-        destroyer.stop()
+    if press_sensor1.pressed() or press_sensor2.pressed():
+        break
+ """
