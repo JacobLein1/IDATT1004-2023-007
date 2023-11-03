97256d06f1a9a87ff3cf5101afed47ca63725a1f
Anders <anders.morille@gmail.com>
2023-11-03 13:19:26 +0100

    Fixed the rust

--- lego_bil/noodle_pong_rust/src/main.rs
+++ lego_bil/noodle_pong_rust/src/main.rs
@@ -24,18 +24,12 @@ fn main() -> Ev3Result<()> {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
+    /*  let buf_reader = BufReader::new(&mut stream);
     let http_request: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("Request: {:#?}", http_request);
-}
-
-#[derive(Serialize, Deserialize)]
-struct AdjustemtRequest {
-    power: float32,
-    horizontal: float32,
+    println!("Request: {:#?}", http_request); */
 }
