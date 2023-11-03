f0f00b9804b0502f8fecea119dc3fafe51ddfead
Jonathan Jensen <jonathan.obsidion@gmail.com>
2023-11-03 15:01:54 +0100

    diawojdwa

--- lego_bil/noodle_pong_rust/src/constants.rs
+++ lego_bil/noodle_pong_rust/src/constants.rs
@@ -1,4 +1,4 @@
-const IP_ADDRESS: &'static str = "192.168.2.2";
+pub const IP_ADDRESS: &'static str = "192.168.2.2";
 const SUBNET_MASK: &'static str = "255.255.0.0";
 const IPV6_ADDRESS: &'static str = "fd7a:da33:f74a:aedb:ca:352b:d122:f18e";
-const PORT: &'static str = "7878";
+pub const PORT: &'static str = "7878";
--- lego_bil/noodle_pong_rust/src/main.rs
+++ lego_bil/noodle_pong_rust/src/main.rs
@@ -5,14 +5,18 @@ use ev3dev_lang_rust::sensors::{SensorPort, TouchSensor};
 use ev3dev_lang_rust::Ev3Result;
 use serde_json::*;
 use std::io::BufReader;
+use std::io;
+use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+
+use constants::*;
 
 fn main() -> Ev3Result<()> {
     let motor_right = LargeMotor::get(MotorPort::OutD)?;
     let motor_left = LargeMotor::get(MotorPort::OutC)?;
     let press_sensor = TouchSensor::get(SensorPort::In1)?;
 
-    let listener = TcpListener::bind("127.0.0.1:7878")?;
+    let listener = TcpListener::bind(IP_ADDRESS + ":" + PORT)?;
 
     for stream in listener.incoming() {
         handle_connection(stream.unwrap());
@@ -23,13 +27,13 @@ fn main() -> Ev3Result<()> {
     Ok(())
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    /*  let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-
-    println!("Request: {:#?}", http_request); */
+fn handle_connection(mut stream: TcpStream) -> io::Result<()> {
+    let mut buffer = [0; 1024];
+    let request = stream.read(&mut buffer)?;
+    dbg!(request);
+    stream.write(OK.as_bytes())?;
+    stream.flush()?;
+    Ok(())
 }
+
+const OK: &str = "HTTP/1.1 200 OK\r\n\r\n";
